f238f9ef6cf9c2c687a6689a33efc8eb74eef6eb
David Barker <dave@ncomtech.com>
2023-05-29 13:28:08 -0400

    fixed bug where 2nd negative number was included in the sum

--- g4g/kadanes.js
+++ g4g/kadanes.js
@@ -15,12 +15,17 @@ class Solution {
             num_ifs++;
             if (sum > max) {
                 max = sum;
+                num_ifs++;
+                if (max <= 0) {
+                    sum = 0;
+                }
             } else {
                 num_ifs++;
                 if (sum <= 0) {
                     sum = 0;
                 }
             }
+            //console.log("val: " + val + ", sum: " + sum + ", max: " + max);
         });
         console.log(num_ifs + " comparisons");
         return max;
@@ -33,6 +38,9 @@ let tests = [
     { "array": [-3, 1, 2, 10, -8, 4, -4], "N": 7},
     { "array": [-3, 1, 2, 10, -16, 4, 10, -4], "N": 8},
     { "array": [-3, 1, 12, 1, -4, 1, -2, 10, -4], "N": 9},
+    { "array": [-81, -71,  98, 55, 76, -52, 68, 15, -77, 77,
+                -42, -70, -53, 86, 29, -30, 14, 25, -94, 73,
+                -68,  81,  44], "N": 23 },
 ];
 
 let solution = new Solution();
