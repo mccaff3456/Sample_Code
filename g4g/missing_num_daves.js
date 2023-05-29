e522101f88f6401b51a593f35befb57e97c95560
David Barker <dave@ncomtech.com>
2023-05-29 09:32:32 -0400

    completed

--- g4g/missing_num_daves.js
+++ g4g/missing_num_daves.js
@@ -1,5 +1,8 @@
-let arr = [3, 6, 2, 1, 5]; // N = 6
+let N = 6;
+let arr = [3, 6, 2, 1, 5];
 
 let sum = arr.reduce((a,b) => (a+b));
 
 console.log("arr " + arr + ", sum = " + sum);
+
+console.log((N*(N+1)/2) - sum);
