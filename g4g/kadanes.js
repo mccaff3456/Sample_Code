f4e4b03927e0d89815079b3e27f3c7ebeeba8d98
David Barker <dave@ncomtech.com>
2023-05-29 10:24:08 -0400

    kadanes stub

---
+++ g4g/kadanes.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number[]} arr
+ * @param {number} N
+ * @returns {number} 
+*/
+class Solution {
+    //Function to find the sum of contiguous subarray with maximum sum.
+    maxSubarraySum(arr, N){
+        return N;
+    } 
+}
+
+let tests = [
+    { "array": [1, 2, 3, -2, 5], "N": 5},
+    { "array": [-1, -2, -3, -4], "N": 4},
+    { "array": [-3, 1, 2, 10, -8, 4, -4], "N": 7},
+];
+
+let solution = new Solution();
+
+tests.forEach ((test) => {
+    console.log("N " + test["N"] + ", (" + test["array"]
+     + ") => [" + solution.maxSubarraySum(test["array"], test["N"]) + "]");
+});
