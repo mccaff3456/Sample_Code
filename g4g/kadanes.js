a1e71740371c49038eb26a1ff6228e32f16aa313
David Barker <dave@ncomtech.com>
2023-05-29 12:07:17 -0400

    reducing comparisons

--- g4g/kadanes.js
+++ g4g/kadanes.js
@@ -6,7 +6,25 @@
 class Solution {
     //Function to find the sum of contiguous subarray with maximum sum.
     maxSubarraySum(arr, N){
-        return N;
+        let max = arr[0];
+
+        function get_max_from_inner(inner) {
+            let temp_sum = 0;
+            inner.forEach((val) => {
+                temp_sum += val;
+                console.log("max: " + max + ", val: " + val + ", sum: " + temp_sum);
+                max = temp_sum > max ? temp_sum : max;
+            })
+        }
+
+        for (let start=0; start < N; start++) {
+            if (arr[start] > 0) {
+                let temp_max = get_max_from_inner(arr.slice(start));
+            } else {
+                console.log("start: " + arr[start]);
+            }
+        }
+        return max;
     } 
 }
 
@@ -14,6 +32,7 @@ let tests = [
     { "array": [1, 2, 3, -2, 5], "N": 5},
     { "array": [-1, -2, -3, -4], "N": 4},
     { "array": [-3, 1, 2, 10, -8, 4, -4], "N": 7},
+    { "array": [-3, 1, 2, 10, -16, 4, 10, -4], "N": 8},
 ];
 
 let solution = new Solution();
